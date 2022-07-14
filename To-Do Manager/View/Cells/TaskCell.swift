f31ecde460f54c89076f9cb99d898ef10a610f2d
Averiser <averiser@gmail.com>
2022-07-14 20:18:56 +0300

    third commit - prototype cell with Horizontal stack view and custom class

---
+++ To-Do Manager/View/Cells/TaskCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCell.swift
+//  To-Do Manager
+//
+//  Created by MyMacBook on 14.07.2022.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+  @IBOutlet var symbol: UILabel!
+  @IBOutlet var title: UILabel!
+    
+}
