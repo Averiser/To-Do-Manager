bb7be6f04b1d4495302bbd4d453417e774727f16
Averiser <averiser@gmail.com>
2022-07-14 16:08:09 +0300

    first commit - general info

---
+++ To-Do Manager/Model/Task.swift
@@ -0,0 +1,38 @@
+//
+//  Task.swift
+//  To-Do Manager
+//
+//  Created by MyMacBook on 14.07.2022.
+//
+
+import Foundation
+
+// task type
+enum TaskPriority {
+  // current
+  case normal
+  // important
+  case important
+}
+
+// task status
+enum TaskStatus {
+  // planned
+  case planned
+  // finished
+  case completed
+}
+
+//  requirements to type describing substance "Task"
+protocol TaskProtocol {
+  var title: String {get set}
+  var type: TaskPriority {get set}
+  var status: TaskStatus {get set}
+}
+
+// substance "Task"
+struct Task: TaskProtocol {
+  var title: String
+  var type: TaskPriority
+  var status: TaskStatus
+}
---
+++ To-Do Manager/Model/TasksStorage.swift
@@ -0,0 +1,34 @@
+//
+//  TasksStorage.swift
+//  To-Do Manager
+//
+//  Created by MyMacBook on 14.07.2022.
+//
+
+import Foundation
+
+protocol TasksStorageProtocol {
+  func loadTasks() -> [TaskProtocol]
+  func saveTasks(_ tasks: [TaskProtocol])
+}
+
+class TasksStorage: TasksStorageProtocol {
+  func loadTasks() -> [TaskProtocol] {
+    // temporal realization which returns test task collection
+    let testTasks: [TaskProtocol] = [
+      Task(title: "Buy food", type: .normal, status: .planned),
+      Task(title: "Wash cat", type: .important, status: .planned),
+      Task(title: "pay off debt", type: .important, status: .completed),
+      Task(title: "Buy vacuum cleaner", type: .normal, status: .completed),
+      Task(title: "Give flowers", type: .important, status: .planned),
+      Task(title: "Make phone call", type: .important, status: .planned)
+    ]
+    return testTasks
+  }
+  
+  func saveTasks(_ tasks: [TaskProtocol]) {
+    <#code#>
+  }
+  
+  
+}
