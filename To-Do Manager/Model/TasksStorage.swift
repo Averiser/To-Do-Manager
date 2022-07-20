745e86a852071deddd8ddb457421f3e7b2d2726e
Averiser <averiser@gmail.com>
2022-07-20 13:30:52 +0300

    ninth commit - saving newly added tasks after reloading

--- To-Do Manager/Model/TasksStorage.swift
+++ To-Do Manager/Model/TasksStorage.swift
@@ -13,23 +13,56 @@ protocol TasksStorageProtocol {
 }
 
 class TasksStorage: TasksStorageProtocol {
+  // Ссылка на хранилище
+  private var storage = UserDefaults.standard
+  // Ключ, по которому будет происходить сохранение и загрузка хранилища из User Defaults
+  var storageKey: String = "tasks"
+  
+  // Перечисление с ключами для записи в User Defaults
+  private enum TaskKey: String {
+    case title
+    case type
+    case status
+  }
+
   func loadTasks() -> [TaskProtocol] {
     // temporal realization which returns test task collection
-    let testTasks: [TaskProtocol] = [
-      Task(title: "Buy food", type: .normal, status: .planned),
-      Task(title: "Wash cat", type: .important, status: .planned),
-      Task(title: "pay off debt", type: .important, status: .completed),
-      Task(title: "Buy vacuum cleaner", type: .normal, status: .completed),
-      Task(title: "Give flowers", type: .important, status: .planned),
-      Task(title: "Make phone call", type: .important, status: .planned),
-      Task(title: "Invite to a party Dolf, Vika, Jeniffer, Christine, Ludwig, Thomas", type: .important, status: .planned)
-    ]
+    var resultTasks: [TaskProtocol] = []
+    let tasksFromStorage = storage.array(forKey: storageKey) as? [[String:String]] ?? []
+    for task in tasksFromStorage {
+      guard let title = task[TaskKey.title.rawValue],
+            let typeRaw = task[TaskKey.type.rawValue],
+            let statusRaw = task[TaskKey.status.rawValue] else {
+              continue
+            }
+//      let type: TaskPriority = typeRaw == "important" ? .important : .normal
+      
+      let type: TaskPriority = typeRaw == "important" ? .important : .normal
+      let status: TaskStatus = statusRaw == "planned" ? .planned : .completed
+      resultTasks.append(Task(title: title, type: type, status: status))
+    }
     
-    return testTasks
+//      Task(title: "Buy food", type: .normal, status: .planned),
+//      Task(title: "Wash cat", type: .important, status: .planned),
+//      Task(title: "pay off debt", type: .important, status: .completed),
+//      Task(title: "Buy vacuum cleaner", type: .normal, status: .completed),
+//      Task(title: "Give flowers", type: .important, status: .planned),
+//      Task(title: "Make phone call", type: .important, status: .planned),
+//      Task(title: "Invite to a party Dolf, Vika, Jeniffer, Christine, Ludwig, Thomas", type: .important, status: .planned)
+    
+    return resultTasks
   }
   
   func saveTasks(_ tasks: [TaskProtocol]) {
-    
+    var arrayForStorage: [[String: String]] = []
+    tasks.forEach { task in
+      var newElementForStorage: Dictionary<String, String> = [:]
+      newElementForStorage[TaskKey.title.rawValue] = task.title
+      newElementForStorage[TaskKey.type.rawValue] = (task.type == .important) ? "important" : "normal"
+      newElementForStorage[TaskKey.status.rawValue] = (task.status == .planned) ? "planned" : "completed"
+      arrayForStorage.append(newElementForStorage)
+    }
+    storage.set(arrayForStorage, forKey: storageKey)
   }
   
   
--- To-Do Manager/TaskListController.swift
+++ To-Do Manager/TaskListController.swift
@@ -24,6 +24,12 @@ class TaskListController: UITableViewController {
           return task1position < task2position
         }
       }
+      // сохранение задач
+      var savingArray: [TaskProtocol] = []
+      tasks.forEach { _, value in
+        savingArray += value
+      }
+      tasksStorage.saveTasks(savingArray)
     }
   }
   
@@ -38,8 +44,8 @@ class TaskListController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        loadTasks()
+//      Не хватает указания на удаление строки (сейчас закомментирована) из viewDidLoad, иначе приложение продолжает затирать данные 
+//        loadTasks()
       // кнопка активации режима редактирования
       navigationItem.leftBarButtonItem = editButtonItem
     }
@@ -262,6 +268,19 @@ class TaskListController: UITableViewController {
         tasks[type]?.append(newTask)
         tableView.reloadData()
       }
+    }
+  }
+  
+  // получение списка задач, их разбор и установка в свойство tasks
+  func setTasks(_ tasksCollection: [TaskProtocol]) {
+    // подготовка коллекции с задачами
+    // будем использовать только те задачи, для которых определена секция
+    sectionsTypesPosition.forEach { taskType in
+      tasks[taskType] = []
+    }
+    // загрузка и разбор задач из хранилища
+    tasksCollection.forEach { task in
+      tasks[task.type]?.append(task)
     }
   }
   
